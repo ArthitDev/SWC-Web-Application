7ae4f65492ec52404273005419c732de04695460
ArthitDev <rthit096@gmail.com>
2024-08-01 16:21:38 +0700

    feat: add 3 theme files colors , theme , typography

---
+++ src/theme/colors.ts
@@ -0,0 +1,66 @@
+const COLORS = {
+  primary: {
+    main: '#043673',
+    sec: '#E3EFFF',
+  },
+  secondary: {
+    main: '#FFFFFF',
+  },
+  text: {
+    primary: '#1C1C1E',
+  },
+  divider: '#F2F2F7',
+  black: '#000',
+  white: {
+    1: '#fff',
+    2: '#f5f5f9',
+    3: '#ffffff',
+  },
+  gray: {
+    1: '#e0e0e0',
+    2: '#d6d6d6',
+    3: '#8E8E93',
+    6: '#F2F2F780',
+    7: '#636366',
+    8: '#48484A',
+    9: '#D1D1D6',
+    10: '#3A3A3C',
+  },
+  blue: {
+    0: '#E5E5EA',
+    1: '#F4F6F9',
+    5: '#486B9F',
+  },
+  red: {
+    4: '#FF3B30',
+    5: 'rgb(177, 35, 29)',
+  },
+  green: {
+    5: 'rgba(25, 140, 53, 1)',
+    6: '#34C759',
+  },
+  orange: {
+    1: '#fff3cc',
+    2: '#ffe899',
+    3: '#ffdc66',
+    4: '#ffd132',
+    5: 'rgba(255, 149, 0, 1)',
+    6: '#FF9500',
+  },
+  transparent: {
+    1: 'rgba(0,0,0,0.1)',
+    2: 'rgba(0,0,0,0.3)',
+    3: 'rgba(0,0,0,0.5)',
+    4: 'rgba(0,0,0,0.7)',
+    5: 'rgba(0,0,0,0.9)',
+  },
+  mainColor: {
+    crimson: '#B1231D',
+    oragne: '#FF9500',
+    blue: '#008AFE',
+    green: '#198C35',
+    gray: '#8E8E93',
+  },
+};
+
+export default COLORS;
