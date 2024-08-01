3ca4b18cc92997ee3ba7fd07f91858562644e1ef
ArthitDev <rthit096@gmail.com>
2024-08-01 15:48:58 +0700

    feat: delete index form homepage and landing

--- src/pages/Homepage/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type HomepageProps = {};
-
-const Homepage: React.FC<HomepageProps> = () => {
-  return <div>Homepage</div>;
-};
-
-export default Homepage;
--- src/pages/Landing/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type LandingProps = {};
-
-const Landing: React.FC<LandingProps> = () => {
-  return <div>Landing</div>;
-};
-
-export default Landing;
