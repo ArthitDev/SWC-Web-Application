1f69c5c4f274f4efd613764ce0444fc7850284a0
ArthitDev <rthit096@gmail.com>
2024-08-01 16:03:46 +0700

    feat: add cache provider and warp components for use reace query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,35 @@
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import createEmotionCache from 'theme/createEmotionCache';
+import theme from 'theme/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const clientSideEmotionCache = createEmotionCache();
+const queryClient = new QueryClient();
+
+type MyAppProps = AppProps & {
+  emotionCache?: EmotionCache;
+};
+
+export default function MyApp({ Component, pageProps }: MyAppProps) {
+  const emotionCache = clientSideEmotionCache;
   return (
     <>
-      <Head>
-        <title>NextJS + Typescript</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>SWC Web Application</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </ThemeProvider>
+      </CacheProvider>
     </>
   );
 }
-
-export default MyApp;
