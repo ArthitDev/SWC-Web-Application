f23802c9a7737edc052209b60ff7e669d9f11c61
ArthitDev <rthit096@gmail.com>
2024-08-09 01:11:48 +0700

    feat: add config pageTitle for tabname header

---
+++ src/config/pageTitles.ts
@@ -0,0 +1,13 @@
+const pageTitles: { [key: string]: string } = {
+  '/': 'SWC Web Application',
+  '/admin': 'SWC Admin  |  Wound',
+  '/admin/articles': 'SWC Admin  |  Articles',
+  '/admin/trick': 'SWC Admin  |  Trick',
+  '/admin/didyouknow': 'SWC Admin  |  Did You Know',
+  '/login': 'SWC Admin Login',
+  '/login/request-reset-password': 'Request Reset Password',
+  '/login/reset-password': 'Reset Password',
+  '/registeradmin': 'SWC Admin Register',
+};
+
+export default pageTitles;
